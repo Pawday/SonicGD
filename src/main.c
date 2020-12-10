7688c96ed3a559678015ad0aea8b762d9f640020
Pawday <pawday@mail.ru>
2020-12-10 21:22:56 +0300

    Switched to MS Nmake and add glew lib

--- src/main.c
+++ src/main.c
@@ -1,16 +1,39 @@
+#include <glew.h>
 #include <stdio.h>
 #include <glfw3.h>
-#include <GL/gl.h>
+
+
+void callback(GLFWwindow* window, int w, int h)
+{
+    glViewport(0,0,w,h);
+    printf("%d %d\n",h,w);
+};
+
+
 int main()
 {
+    
+    //init glfw
     if (!glfwInit())
         return -1;
+
 
     GLFWwindow* window = glfwCreateWindow(1080,720,"",0,0);
 
 
     glfwShowWindow(window);
     glfwMakeContextCurrent(window);
+
+    //init glew
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        /* Problem: glewInit failed, something is seriously wrong. */
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        return -1;
+    }
+
+    glfwSetWindowSizeCallback(window,callback);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -21,7 +44,6 @@ int main()
         glfwPollEvents();
     }
     
-
 
     glfwTerminate();
 }
